f35afc438f0a5a7c39a268c2e8587f8452926a31
Kameron B <56085171+Kameron2442@users.noreply.github.com>
2020-03-25 17:52:12 -0400

    Changed name

--- app/src/main/java/com/bourgeois/lister/MainActivity.java
+++ app/src/main/java/com/bourgeois/lister/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.myapplication;
+package com.bourgeois.lister;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.bourgeois.lister.R;
 
 public class MainActivity extends AppCompatActivity {
 
